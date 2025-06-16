4d37281de0c5edeaaa3490fae9b1d27c57127168
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-06-16 23:01:00 +0600

    Adding the Mongodb Configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -9,10 +10,36 @@ app.use(express.json());
 
 //routes
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send("Welcome to Vouch Vault API");
 })
 
-app.listen(port,()=>{
+//mongodb connection
+// VouchVaultManager
+// jdDl8hd8zHzkVsHg
+const uri = "mongodb+srv://VouchVaultManager:jdDl8hd8zHzkVsHg@cluster0.pv5o1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
