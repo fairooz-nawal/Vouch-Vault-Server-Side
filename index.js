7ab44c7d5914ecc9d85711176b7c5fedbeb64d4f
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-06-16 23:13:17 +0600

    The MongoDB credentials are secure

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -13,11 +14,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Welcome to Vouch Vault API");
 })
-
+console.log(process.env.DB_USER)
 //mongodb connection
-// VouchVaultManager
-// jdDl8hd8zHzkVsHg
-const uri = "mongodb+srv://VouchVaultManager:jdDl8hd8zHzkVsHg@cluster0.pv5o1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pv5o1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
