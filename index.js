93dbc325d34d84cf5da09223510452d189d9bde2
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-06-23 00:55:44 +0600

    Adding the Delete API of the review

--- index.js
+++ index.js
@@ -104,6 +104,14 @@ async function run() {
             res.send(result);
         })
 
+        // Delete review API
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await review.deleteOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
